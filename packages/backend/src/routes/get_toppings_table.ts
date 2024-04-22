d58c2ae7afd191f8c2aaf33b28dafa579d8bcee3
tamaki12345 <tamakiharuta95@gmail.com>
2024-04-22 13:46:56 +0900

    #28 commitするファイルを訂正

--- packages/backend/src/routes/get_toppings_table.ts
+++ packages/backend/src/routes/get_toppings_table.ts
@@ -1,69 +1,51 @@
-import express, { Request } from 'express';
+import express from 'express';
 import prisma from '@/lib/prismaClient.js';
-
-interface Topping {
-  id: number,
-  amount: number,
-}
-
-interface PriceBody {
-  size: number,
-  toppings: Topping[] | undefined,
-}
-
-interface RequestBody {
-  isOnlyAvailable: boolean,
-}
 
 const router = express.Router();
 
-router.get('/price', async (req: Request<any, any, RequestBody>, res, next) => {
-  const isOnlyAvailabe = req.body.isOnlyAvailable;
-
-  if (isOnlyAvailabe) {
-    try {
-      const topping_table = await prisma.toppings.findMany({
-        where: {
-          available: true,
-        },
-        select: {
-          id: true,
-          label: true
-        }
-      })
-
-      if (!topping_table) {
-        next();
-        return;
+router.get('/koujirer/toppingList/available', async (req, res, next) => {
+  try {
+    const topping_table = await prisma.toppings.findMany({
+      where: {
+        available: true,
+      },
+      select: {
+        id: true,
+        label: true
       }
+    });
 
-      res.status(200).send({ topping_table });
-
-    } catch(e) {
+    if (!topping_table) {
       next();
-      console.error(e);
+      return;
     }
 
-  } else {
-    try {
-      const topping_table = await prisma.toppings.findMany({
-        select: {
-          id: true,
-          label: true
-        }
-      })
+    res.status(200).send({ topping_table });
 
-      if (!topping_table) {
-        next();
-        return;
+  } catch(e) {
+    next();
+    console.error(e);
+  }
+});
+
+router.get('/koujirer/toppingList/all', async (req, res, next) => {
+  try {
+    const topping_table = await prisma.toppings.findMany({
+      select: {
+        id: true,
+        label: true
       }
-
-      res.status(200).send({ topping_table });
-
-    } catch(e) {
+    });
+     if (!topping_table) {
       next();
-      console.error(e);
+      return;
     }
+
+    res.status(200).send({ topping_table });
+
+  } catch(e) {
+    next();
+    console.error(e);
   }
 });
 
