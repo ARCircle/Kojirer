b0b8c2c5190b092585418d06833269ca81951439
TOFU <4416kidr@gmail.com>
2024-06-28 12:20:24 +0900

    add: 画面サイズごとに適切なフォントサイズ、スペースを設定した

--- packages/frontend/src/pages/yobidashi.tsx
+++ packages/frontend/src/pages/yobidashi.tsx
@@ -31,8 +31,9 @@ type FudaProps = {
 }
 
 const FudaSimple: React.FC<FudaProps> = ({ order, color }) => {
+  const responsiveFontSize = { base: '0.5em', md: '3em', lg: '3em' };
   return (
-    <Center h='100%' w='100%' bg={color} borderColor='gray' border='4px' borderRadius='16px' fontSize='xxx-large' fontWeight='bold'>{order.id}</Center>
+    <Center h='100%' w='100%' bg={color} borderColor='gray' border='4px' borderRadius='16px' fontSize={responsiveFontSize} fontWeight='bold'>{order.id}</Center>
   )
 }
 
@@ -42,17 +43,18 @@ const FudaProgress: React.FC<FudaProps> = ({ order }) => {
     const numDoneDons = order.dons.filter(don => don.status === 2).length;
     return Math.floor(numDoneDons / numDons * 100);
   }
+  const responsiveFontSize = { base: '0.5em', md: '2em', lg: '2.5em' };
 
   return (
     <Box h='100%' w='100%' pos='relative' borderColor='gray' border='4px' borderRadius='16px' overflow='hidden'>
       <Box h='100%' w={`${calculateProgress(order)}%`} bg='blue.400' pos='absolute' opacity={0.3} ></Box>
       <Flex flexDirection='row' px='10px' h='100%' pos='absolute' w='100%'>
-        <Center w='50%' fontWeight='bold' fontSize='1em' color='black' pr='2px' >{order.id}</Center>
+        <Center w='50%' fontWeight='bold' fontSize={responsiveFontSize} color='black' pr='2px' >{order.id}</Center>
         <Center w='2%'>
           <Center h='90%' color='gray.400' border='2px' borderRadius={4}></Center>
         </Center>
         {/* <Divider orientation='horizontal' /> */}
-        <Center w='50%' fontSize='1em'>{`${order.dons.filter(don => don.status === 2).length}/${order.dons.length}`}</Center>
+        <Center w='50%' fontSize={responsiveFontSize} fontWeight='bold'>{`${order.dons.filter(don => don.status === 2).length}/${order.dons.length}`}</Center>
       </Flex>
     </Box>
   )
@@ -123,6 +125,7 @@ const Yobidashi: React.FC = () => {
   type TableLabelProp = {
     name: string
   }
+  const responsiveSpace = { base: '1px', md: '5px', lg: '10px' }; // https://v2.chakra-ui.com/docs/styled-system/responsive-styles
   const TableLabel: React.FC<TableLabelProp> = ({ name }: TableLabelProp) => {
     return (
       <Heading textAlign='center' p='2px' w='fit-content' mx='auto' fontSize='5xl' fontWeight='bold' >{name}</Heading>
@@ -132,17 +135,17 @@ const Yobidashi: React.FC = () => {
     <>
       <Heading textAlign='center' fontSize='xxx-large'>呼び出し口画面</Heading>
       <Flex flexDirection='row' px='10px'>
-        <Box w='25%' border='2px' borderColor='black' p='20px' mr='5px'>
+        <Box w='25%' border='2px' borderColor='black' p={responsiveSpace} mr='5px'>
           <TableLabel name='調理中' />
           <YobidashiRow n={Math.floor(Math.random() * 16) + 4} status={'cooking'} />
         </Box>
-        <Box w='50%' border='2px' borderColor='black' p='20px' verticalAlign='top' ml='5px'>
+        <Box w='50%' border='2px' borderColor='black' p={responsiveSpace} verticalAlign='top' ml='5px'>
           <TableLabel name='呼出中' />
-          <Grid templateColumns='repeat(4, 1fr)' gap={6} pt='20px'>
+          <Grid templateColumns='repeat(4, 1fr)' gap={responsiveSpace} pt={responsiveSpace}>
             <YobidashiTable n={Math.floor(Math.random() * 32) + 8} status={'calling'} />
           </Grid>
         </Box>
-        <Box w='25%' border='2px' borderColor='black' p='20px' verticalAlign='top' ml='5px'>
+        <Box w='25%' border='2px' borderColor='black' p={responsiveSpace} verticalAlign='top' ml='5px'>
           <TableLabel name='呼出完了' />
           {/* <Grid templateColumns='repeat(2, 1fr)' gap={6} pt='20px'>
             <YobidashiTable n={Math.floor(Math.random()*16)+4} status={'finish'}/>
