9e8466cbf003d468cf49e974d5ca96d4c2f55ab2
Hiroki Ando <105905033+hikuohiku@users.noreply.github.com>
2024-08-12 23:22:32 +0900

    feat: `POST /api/order`：新規注文を追加 のAPIを作成 (#38)
    * add: order.ts orderをPOSTするルートを追加
    
    * fix(backend): Support schema changed in #31
    
    (cherry picked from commit b9bf016b902b31f3aa9e1debd823970950c7fd72)
    
    * up: order.ts エラーレスポンスにリクエストを含むようにした
    
    * up: openapi.json `POST /api/order` のOpenAPI定義を追加
    
    * add: util/bigint2number.ts ^^
    - prisma スキーマと response の処理の中で Bigint から number への変換が頻出するので util 化 ^^
    
    * up: export
    
    * rn: util/typeConverters
    
    * fix: order.ts bigintをstringに変換ではなくnumberに変換するようにした
    
    * fix: order.ts import pathに.jsを追記
    
    * rename `util` -> `utils` `util`ディレクトリの名前を変更
    `util`パッケージと名前が衝突してしまいimportできなかったため
    
    * fix: `OrderCreateResponse.json` "string"なidを"integer"に変更
    
    ---------
    
    Co-authored-by: woodnext <shun20020611@yahoo.co.jp>
    Co-authored-by: taniiicom <mail@taniii.com>

---
+++ packages/backend/src/utils/typeConverters.ts
@@ -0,0 +1,11 @@
+// bigint2number converts a bigint to a number if it is within the safe integer range, otherwise it returns null.
+export function bigint2number(value: bigint): number {
+  const maxSafeInteger = BigInt(Number.MAX_SAFE_INTEGER);
+  const minSafeInteger = BigInt(Number.MIN_SAFE_INTEGER);
+
+  if (value <= maxSafeInteger && value >= minSafeInteger) {
+    return Number(value);
+  } else {
+    throw new Error('BigIntの値がNumberの安全な範囲を超えています');
+  }
+}
