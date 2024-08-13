e44d751ff58c5feea7e2542c00c104b16374b096
tamaki12345 <101048510+tamaki12345@users.noreply.github.com>
2024-08-13 22:12:19 +0900

    feat: toppingのtableを取得するAPIを作成 (#36)
    * fix(backend): Support schema changed in #31
    
    * feat(backend): Add endpoint: '/dons/price'
    
    Closes #19
    
    * #28を実装
    
    * #28 commitするファイルを訂正
    
    * add comment out
    
    * path名 (file名)変更, topping_pricesからpriceを取得
    
    * 動作するように修正
    
    ---------
    
    Co-authored-by: woodnext <shun20020611@yahoo.co.jp>
    Co-authored-by: tamaki12345 <tamakihaurta95@gmail.com>

---
+++ packages/backend/src/routes/toppings.ts
@@ -0,0 +1,89 @@
+import express from 'express';
+import prisma from '@/lib/prismaClient.js';
+
+const router = express.Router();
+
+//get available only
+router.get('/available', async (req, res, next) => {
+  try {
+    const toppings = await prisma.toppings.findMany({
+      
+      //get available: true
+      where: {
+        available: true,
+      },
+
+      //include topping_prices table
+      include: {
+        topping_prices: {
+          select: {
+            price: true,
+          },
+          orderBy: { since: 'desc' },
+          take: 1
+        }
+      }
+    });
+
+    if (!toppings) {
+      next();
+      return;
+    }
+
+    const sendToppings = toppings.map(t => {
+      const { available, topping_prices, ...sendTopping } = t
+
+      return {
+        ...sendTopping,
+        price: topping_prices[0].price
+      }
+    })
+
+    res.status(200).send(sendToppings);
+
+  } catch(e) {
+    next();
+    console.error(e);
+  }
+});
+
+//get all
+router.get('/all', async (req, res, next) => {
+  try {
+    const toppings = await prisma.toppings.findMany({
+
+      //include topping_prices table
+      include: {
+        topping_prices: {
+          select: {
+            price: true,
+          },
+          orderBy: { since: 'desc' },
+          take: 1
+        }
+      },
+    });
+
+    if (!toppings) {
+      next();
+      return;
+    }
+
+    const sendToppings = toppings.map(t => {
+      const { available, topping_prices, ...sendTopping } = t
+
+      return {
+        ...sendTopping,
+        price: topping_prices[0].price
+      }
+    })
+
+    res.status(200).send(sendToppings);
+
+  } catch(e) {
+    next();
+    console.error(e);
+  }
+});
+
+export default router;
