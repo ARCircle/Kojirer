b245961c37387772ac446630e3eff1ce9056ff84
tenk-laptop <takuo6000@gmail.com>
2024-10-27 02:38:38 +0000

    kitchen UIを実装 見た目だけで，機能はまだです．

--- packages/frontend/src/pages/index.tsx
+++ packages/frontend/src/pages/index.tsx
@@ -1,11 +1,13 @@
-import { Button, Link } from "@chakra-ui/react";
+import { Link } from "@/router";
+import { Button } from "@chakra-ui/react";
 
 export default function Home() {
   return (
     <>
       <h1>Home</h1>
       <Button>Button</Button>
-      <Link href='/yobidashi'>yobidashi</Link>
+      <Link to='/yobidashi'>yobidashi</Link>
+      <Link to='/kitchen'>kitchen</Link>
     </>
   );
 }
