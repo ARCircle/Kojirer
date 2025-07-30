e8821a0b7ac11cb76fd8d9d27e40aea2b4fa940d
taniiicom <mail@taniii.com>
2025-07-30 17:58:49 +0900

    up: [uiux][design][animation] StatusPage > 刷新

--- packages/frontend/src/pages/index.tsx
+++ packages/frontend/src/pages/index.tsx
@@ -3,13 +3,19 @@ import { Button } from '@/components/ui/button';
 
 export default function Home() {
   return (
-    <div className="p-4 space-y-4">
-      <h1 className="text-2xl font-bold">Home</h1>
+    <div className='p-4 space-y-4'>
+      <h1 className='text-2xl font-bold'>Home</h1>
       <Button>Button</Button>
-      <div className="space-y-2">
-        <Link to='/yobidashi' className="block text-blue-600 hover:underline">yobidashi</Link>
-        <Link to='/kitchen' className="block text-blue-600 hover:underline">kitchen</Link>
-        <Link to='/uketuke' className="block text-blue-600 hover:underline">受付画面</Link>
+      <div className='space-y-2'>
+        <Link to='/status' className='block text-blue-600 hover:underline'>
+          yobidashi
+        </Link>
+        <Link to='/kitchen' className='block text-blue-600 hover:underline'>
+          kitchen
+        </Link>
+        <Link to='/uketuke' className='block text-blue-600 hover:underline'>
+          受付画面
+        </Link>
       </div>
     </div>
   );
