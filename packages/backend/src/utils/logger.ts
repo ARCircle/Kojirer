143736230931a4fecc5a2ed9a52208bc41c97500
hikuohiku <hikuohiku@gmail.com>
2025-08-18 23:41:37 +0900

    devモードのloggerでオブジェクトをprintするように

--- packages/backend/src/utils/logger.ts
+++ packages/backend/src/utils/logger.ts
@@ -16,8 +16,9 @@ const devFormat = winston.format.combine(
   }),
   winston.format.errors({ stack: true }),
   winston.format.colorize(),
-  winston.format.printf(({ timestamp, level, message, stack }) => {
-    return `${timestamp} [${level}]: ${message}${stack ? `\n${stack}` : ''}`;
+  winston.format.printf(({ timestamp, level, message, stack, ...metadata }) => {
+    const metaString = Object.keys(metadata).length > 0 ? `: ${JSON.stringify(metadata)}` : '';
+    return `${timestamp} [${level}]: ${message}${metaString}${stack ? `\n${stack}` : ''}`;
   }),
 );
 
